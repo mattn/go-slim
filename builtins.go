a24a8cd4ac87ca0c7fb11c4cfa495c72c975b48f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-09-01 00:04:12 +0900

    add repeat

--- builtins.go
+++ builtins.go
@@ -2,17 +2,26 @@ package slim
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-func Trim(v Value) (Value, error) {
-	return strings.TrimSpace(fmt.Sprint(v)), nil
+func Trim(s Value) (Value, error) {
+	return strings.TrimSpace(fmt.Sprint(s)), nil
 }
 
-func ToUpper(v Value) (Value, error) {
-	return strings.ToUpper(fmt.Sprint(v)), nil
+func ToUpper(s Value) (Value, error) {
+	return strings.ToUpper(fmt.Sprint(s)), nil
 }
 
-func ToLower(v Value) (Value, error) {
-	return strings.ToLower(fmt.Sprint(v)), nil
+func ToLower(s Value) (Value, error) {
+	return strings.ToLower(fmt.Sprint(s)), nil
+}
+
+func Repeat(s Value, n Value) (Value, error) {
+	i, err := strconv.ParseInt(fmt.Sprint(n), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Repeat(fmt.Sprint(s), int(i)), nil
 }
