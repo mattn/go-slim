00dbebaf40b4f1ab7779dc4568730f4281faab4f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-09-01 01:48:43 +0900

    better not to use log package

--- cmd/goslim/main.go
+++ cmd/goslim/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/mattn/go-slim"
 )
 
+func fatalIf(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	t, err := slim.Parse(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
-	}
-	t.Execute(os.Stdout, nil)
+	fatalIf(err)
+	err = t.Execute(os.Stdout, nil)
+	fatalIf(err)
 }
