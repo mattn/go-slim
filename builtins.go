b9efb59b851402482acb0cafb3e64adb5d0dd9bc
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-08-31 10:41:56 +0900

    add ToUpper, ToLower

--- builtins.go
+++ builtins.go
@@ -8,3 +8,11 @@ import (
 func Trim(v Value) (Value, error) {
 	return strings.TrimSpace(fmt.Sprint(v)), nil
 }
+
+func ToUpper(v Value) (Value, error) {
+	return strings.ToUpper(fmt.Sprint(v)), nil
+}
+
+func ToLower(v Value) (Value, error) {
+	return strings.ToLower(fmt.Sprint(v)), nil
+}
